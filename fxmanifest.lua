8b998ff084711bf94c16a165814fcfea5a882b01
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2024-12-17 21:26:14 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
 author 'Tinus_NL'
 description 'Tinus Flatbed'
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}
 
 client_scripts {
     'Config.lua',
